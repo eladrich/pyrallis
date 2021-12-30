72b089ea0481d8e273f0d6966968a639048f72c9
eladrich <elad.richardson@gmail.com>
2021-12-30 17:53:42 +0200

    Added missing pyrallis.field and version bump :sunglasses:

--- pyrallis/__init__.py
+++ pyrallis/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from . import wrappers, utils
 from pyrallis.help_formatter import SimpleHelpFormatter
 from pyrallis.argparsing import ArgumentParser, ParsingError, wrap
 from .parsers.encoding import encode, dump
 from .parsers.decoding import decode, load
+from .fields import field

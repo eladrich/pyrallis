46a3a63621da9691570ae7b9a3c379bf8678b292
Hiroyuki Deguchi <deguchi.hiroyuki.db0@is.naist.jp>
2022-06-09 12:12:18 +0900

    Add option alias

--- pyrallis/wrappers/field_wrapper.py
+++ pyrallis/wrappers/field_wrapper.py
@@ -135,6 +135,9 @@ class FieldWrapper(Wrapper[dataclasses.Field]):
         """
         return self.field.metadata.get("custom_args", {})
 
+    @property
+    def option_aliases(self) -> List[str]:
+        return self.field.metadata.get("aliases", [])
 
     @property
     def option_strings(self) -> List[str]:
@@ -164,8 +167,9 @@ class FieldWrapper(Wrapper[dataclasses.Field]):
         options.append(self.dest)
 
         # remove duplicates by creating a set.
-        option_strings = set(f"{dash}{option}" for dash, option in zip(dashes, options))
-        return list(sorted(option_strings, key=len))
+        option_strings = list(set(f"{dash}{option}" for dash, option in zip(dashes, options)))
+        option_strings.extend(list(set(self.option_aliases)))
+        return option_strings
 
     @property
     def dest(self) -> str:
